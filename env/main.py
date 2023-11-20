6dfa227babff5d7c92a386fe74a108d1d6615206
Vitor Pinto Canadas <vitorpinto@MacBook-Pro-de-Vitor.local>
2023-11-20 12:58:52 +0100

    list

--- env/main.py
+++ env/main.py
@@ -9,6 +9,8 @@ from kivymd.uix.label import MDLabel
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.textfield import MDTextField
+from kivymd.uix.list import OneLineListItem
+
 import mysql.connector
 
 # Design
@@ -16,6 +18,7 @@ KV = '''
 ScreenManager:
     LoginScreen:
     WelcomeScreen:
+    BetDetailScreen:
 
 <LoginScreen>:
     name: 'login'
@@ -86,15 +89,8 @@ ScreenManager:
                 valign: 'center'  # Alinea el texto en el centro vertical
        
         ScrollView:
-            MDLabel:
-                id: bet_teams_label
-                text: ''
-                size_hint_y: None
-                height: self.texture_size[1]
-                theme_text_color: 'Primary'
-                font_style: 'H4'
-                valign: 'top'
-                halign: 'center'
+            MDList:
+                id: bets_list
                 
                     
 
@@ -105,20 +101,28 @@ ScreenManager:
             size_hint_y: None
             height: dp(50)
             pos_hint: {"center_x": .5, "center_y": .5}
-
-
+<BetDetailScreen>:
+    name: 'bet_detail'
+    
+    MDLabel: 
+        id: detail_label
+        text: ''
+        halign: 'center'
+        size_hint_y: None
+        height: self.texture_size[1]
 '''
 
 class LoginScreen(Screen):
     pass
 
 class WelcomeScreen(Screen):
-    user_bets = [] 
-    def on_pre_enter(self):
-        # Obtener el ID de usuario desde el inicio de sesión
-        login_screen = self.manager.get_screen('login')
-        self.ids.bet_teams_label.font_size = '18sp'
-       
+    user_id = None  # Asumiremos que este valor se establece en alguna parte antes de entrar a la pantalla
+
+    def on_pre_enter(self, *args):
+        # Limpiar la lista cada vez que se entra a la pantalla para evitar duplicados
+        self.ids.bets_list.clear_widgets()
+        
+        # Intenta conectarte a la base de datos y obtener las apuestas del usuario
         try:
             # Establecer conexión a la base de datos MySQL
             conn = mysql.connector.connect(
@@ -127,31 +131,45 @@ class WelcomeScreen(Screen):
                 password='PEPE',
                 database='bdsuperbowl'
             )
-
             cursor = conn.cursor()
-            query = """
-                SELECT matchs.id, matchs.equipe1, matchs.equipe2, matchs.jour, matchs.debut, matchs.fin, matchs.score, matchs.statut
-                FROM mises
-                JOIN matchs ON mises.id_match = matchs.id
-                WHERE mises.id_utilisateur = %s
-            """
-            cursor.execute(query, (self.user_id,))
-            user_bets = cursor.fetchall()
+            # Asegúrate de que 'user_id' está definido y no es None
+            if self.user_id is not None:
+                # Utiliza parámetros seguros para prevenir la inyección de SQL
+                cursor.execute("""
+                    SELECT matchs.id, matchs.equipe1, matchs.equipe2, matchs.jour, matchs.debut, 
+                    matchs.fin, matchs.score, matchs.statut
+                    FROM mises
+                    JOIN matchs ON mises.id_match = matchs.id
+                    WHERE mises.id_utilisateur = %s
+                """, (self.user_id,))
+                self.user_bets = cursor.fetchall()
+            else:
+                # Manejar caso en el que 'user_id' no está definido (por ejemplo, mostrar un mensaje de error)
+                self.user_bets = []
 
+            # Cerrar la conexión a la base de datos
             cursor.close()
             conn.close()
 
-            self.user_bets = user_bets
-
-            # Mostrar los nombres de los equipos en el MDLabel
-            bet_teams = '\n\n'.join([
-                f"{row[1]} vs {row[2]} - Date: {row[3]}, Debut: {row[4]}, Fin: {row[5]}, Resultat: {row[6]}, Statut: {row[7]}" 
-                for row in user_bets
-            ])
-            self.ids.bet_teams_label.text = f'{bet_teams}'
-
         except mysql.connector.Error as err:
             print(f"Error: {err}")
+            self.user_bets = []  # Resetear la lista de apuestas en caso de error
+
+        # Actualizar la interfaz de usuario con la lista de apuestas
+        for bet in self.user_bets:
+            # Formatear la información de la apuesta para mostrarla
+            bet_details = f"{bet[1]} vs {bet[2]} - Date: {bet[3]}, Debut: {bet[4]}, Fin: {bet[5]}, Resultat: {bet[6]}, Statut: {bet[7]}"
+            list_item = OneLineListItem(text=bet_details,
+                                        on_release=lambda x, bet_id=bet[0]: self.open_bet_details(bet_id))
+            self.ids.bets_list.add_widget(list_item)
+
+    def open_bet_details(self, bet_id):
+        # Aquí estableces la lógica para abrir la pantalla de detalles de la apuesta
+        # Por ejemplo, puedes pasar el ID de la apuesta a la pantalla de detalles
+        bet_detail_screen = self.manager.get_screen('bet_detail')
+        bet_detail_screen.ids.detail_label.text = f"Details for bet {bet_id}"
+        self.manager.current = 'bet_detail'
+
 
 class MyApp(MDApp):
     def build(self):
@@ -208,6 +226,9 @@ class MyApp(MDApp):
 
     def go_to_login(self):
         self.screen_manager.current = 'login'
+
+class BetDetailScreen(Screen):
+    pass
 
 if __name__ == '__main__':
     MyApp().run()
